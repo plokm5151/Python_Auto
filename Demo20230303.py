34adfb71a9b0414c495183786e3ac0f3f1f110b6
frank <frank.chen@keeda.com.tw>
2023-03-21 13:36:20 +0800

    add Keeda file

---
+++ Demo20230303.py
@@ -0,0 +1,172 @@
+from appium import webdriver
+import time
+import random
+from time import sleep
+
+def random_number():
+ return random.randint(1,999999999999999)
+
+
+class Test:
+ caps = {}
+ caps['deviceName']='RF8N71V1A3N'
+ caps['platformName']='Android'
+ caps['platformVersion']='12'
+ caps['appPackage']='com.example.dev_keeda'
+ caps['appActivity']='com.example.dev_keeda.MainActivity'
+ passWD='aPPLEPLOKM123'
+
+
+#連線至手機
+ driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', caps)
+
+
+#定位登入按鍵並點擊
+ driver.find_element_by_xpath('//android.widget.Button[@content-desc="登入"]').click()
+ time.sleep(3)
+#定位輸入帳號密碼及按下登入
+ driver.find_element_by_xpath(" //*[@text='電子郵件'] ").click()
+ driver.find_element_by_xpath(" //*[@text='電子郵件'] ").send_keys('frank.chen+6@keeda.com.tw')
+
+ time.sleep(2)
+
+#測試顯示密碼功能
+# driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView/android.view.View/android.widget.EditText[2]/android.view.View').click()
+# driver.tap([(901,1061),(1028,1187)],100)
+ driver.find_element_by_xpath(" //*[@text='輸入密碼'] ").click()
+ driver.find_element_by_xpath(" //*[@text='輸入密碼'] ").send_keys(passWD)
+
+ driver.find_element_by_xpath('//android.view.View[@content-desc="或"]').click()
+ time.sleep(2)
+ driver.find_element_by_accessibility_id("登入").click()
+
+#登入之後
+ time.sleep(5)
+ driver.find_element_by_accessibility_id("活動").click()
+ time.sleep(3)
+ driver.find_element_by_accessibility_id("聊天").click()
+ time.sleep(3)
+ driver.find_element_by_accessibility_id("貼文").click()
+ time.sleep(3)
+ driver.find_element_by_accessibility_id("設定").click()
+ time.sleep(3)
+ #點擊個人資料設定還有相片
+ driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.Button[1]').click()
+ driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button[3]').click()
+ time.sleep(2)
+ driver.find_element_by_id('com.android.permissioncontroller:id/permission_allow_button').click()
+ time.sleep(2)
+ #利用座標選取圖片
+ driver.tap([(113,1751),(967,1877)],500)
+ driver.tap([(113,1751),(967,1877)],500)
+ time.sleep(2)
+ #利用座標點擊完成
+ driver.tap([(1006,145)],50)
+ time.sleep(3)
+ #點擊修改id
+ driver.find_element_by_class_name('android.widget.EditText').click()
+ driver.find_element_by_class_name('android.widget.EditText').clear()
+ driver.find_element_by_class_name('android.widget.EditText').send_keys(random_number())
+ driver.tap([(151,418)],50)
+ driver.find_element_by_accessibility_id("儲存").click()
+ time.sleep(2)
+
+ #點進去關於我們查看
+ driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.Button[3]').click()
+ time.sleep(2)
+ driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button[1]').click()
+ time.sleep(1)
+ #點擊貼文頁面
+ driver.find_element_by_accessibility_id("貼文").click()
+ #滑動貼文
+ for i in range(1,10):
+  driver.swipe(533,1655,533,600,500)
+  time.sleep(0.5)
+ time.sleep(2)
+ driver.find_element_by_xpath('//android.view.View[@content-desc="第 2 個分頁 (共 3 個)"]').click()
+ time.sleep(1)
+ #+貼文
+ driver.find_element_by_xpath('//android.widget.Button[@content-desc="貼文"]').click()
+ time.sleep(2)
+ #選擇圖片
+ driver.tap([(212,1012)],1)
+ time.sleep(2)
+ #確定選取
+ driver.find_element_by_id('com.google.android.documentsui:id/action_menu_select').click()
+ time.sleep(2)
+ #輸入貼文內容
+ driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText').click()
+ time.sleep(2)
+ driver.find_element_by_class_name('android.widget.EditText').send_keys('test123')
+ driver.tap([(353,1188)],1)
+ driver.find_element_by_xpath('//android.widget.Button[@content-desc="分享"]').click()
+ time.sleep(5)
+ #切到貼文的第三個分頁
+ driver.find_element_by_xpath('//android.view.View[@content-desc="第 3 個分頁 (共 3 個)"]').click()
+ time.sleep(1)
+ #點選照片
+ driver.find_element_by_xpath('//android.widget.Button[@content-desc="照片"]').click()
+ #選擇圖片
+ driver.tap([(212,1012)],1)
+ time.sleep(1)
+ #確定選取
+ driver.find_element_by_id('com.google.android.documentsui:id/action_menu_select').click()
+ time.sleep(2)
+ #接下來是刪除圖片
+ driver.tap([(300,900)],1000)
+ time.sleep(1)
+ driver.find_element_by_xpath('//android.widget.Button[@content-desc="是"]').click()
+ #切換到聊天頁面
+ driver.find_element_by_xpath('//android.view.View[@content-desc="聊天"]').click()
+
+
+
+
+
+ #點擊新增寵物
+ #driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.Button[2]').click()
+ #time.sleep(2)
+
+
+
+ #點擊設定寵物圖片(這邊要重製成function)
+ #driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.widget.Button[3]').click()
+ #利用座標選取圖片
+ #driver.tap([(113,1751),(967,1877)],500)
+ #driver.tap([(113,1751),(967,1877)],500)
+ #time.sleep(2)
+ #利用座標點擊完成
+ #driver.tap([(1006,145)],50)
+ #time.sleep(3)
+
+ #輸入寵物名字
+ #driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.widget.EditText[1]').click()
+ #driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.widget.EditText[1]').send_keys(random_number())
+ #點擊空白處
+ #driver.tap([(155,446)],50)
+ #點擊寵物生日
+ #driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.view.View[6]').click()
+ #driver.find_element_by_xpath('')
+ #driver.swipe(start_x=367 ,start_y=1872 ,end_x=367 ,end_y= 2093,duration=1000)
+ #time.sleep(1)
+ #driver.find_element_by_xpath('//android.widget.Button[@content-desc="Done"]').click()
+
+
+
+#------------------以下為功能測試區-------------------------------
+#看起來使用文本定位也是可以，不過本程式通常情況下還是使用xpath
+#driver.find_element_by_xpath(" //*[@text='電子郵件'] ").click()
+#driver.find_element_by_xpath(" //*[@text='電子郵件'] ").send_keys('frank.chen+10@keeda.comt.tw')
+#driver.background_app(5) 將app放入後台
+
+#driver.find_elements(By.ID'登入').click()
+#driver.find_element_by_accessibility_id('android.widget.Button')
+#selected_icon=(By.XPATH,'//android.widget.Button[@content-desc="註冊"]')
+#driver.find(selected_icon)
+
+
+#driver.close_app()
+#driver.quit()
+
+
+
---
+++ chatGPT_version.py
@@ -0,0 +1,77 @@
+from appium import webdriver
+import time
+import random
+
+
+class Test:
+    def __init__(self):
+        self.passwd = 'aPPLEPLOKM123'
+        self.driver = None
+        self.caps = {
+            'deviceName': 'RF8N71V1A3N',
+            'platformName': 'Android',
+            'platformVersion': '12',
+            'appPackage': 'com.example.dev_keeda',
+            'appActivity': 'com.example.dev_keeda.MainActivity',
+        }
+
+    def connect_to_device(self):
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', self.caps)
+
+    def random_number(self):
+        return random.randint(1, 999999999999999)
+
+    def login(self):
+        self.driver.find_element_by_xpath('//android.widget.Button[@content-desc="登入"]').click()
+        time.sleep(3)
+        self.driver.find_element_by_xpath(" //*[@text='電子郵件'] ").click()
+        self.driver.find_element_by_xpath(" //*[@text='電子郵件'] ").\
+            send_keys('frank.chen+6@keeda.com.tw')
+        time.sleep(2)
+        self.driver.find_element_by_xpath(" //*[@text='輸入密碼'] ").click()
+        self.driver.find_element_by_xpath(" //*[@text='輸入密碼'] ").\
+            send_keys(self.passwd)
+        self.driver.find_element_by_accessibility_id("登入").click()
+        time.sleep(5)
+
+    def navigate_to_chat(self):
+        self.driver.find_element_by_accessibility_id("活動").click()
+        time.sleep(3)
+        self.driver.find_element_by_accessibility_id("聊天").click()
+        time.sleep(3)
+
+    def navigate_to_post(self):
+        self.driver.find_element_by_accessibility_id("貼文").click()
+        time.sleep(3)
+
+    def navigate_to_settings(self):
+        self.driver.find_element_by_accessibility_id("設定").click()
+        time.sleep(3)
+
+    def navigate_to_personal_settings(self):
+        self.driver.find_element_by_xpath(
+            '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/'
+            'android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/'
+            'android.view.View/android.view.View[1]/android.view.View/android.widget.Button[1]').click()
+        self.driver.find_element_by_xpath(
+            '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/'
+            'android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/'
+            'android.view.View/android.view.View/android.view.View/android.widget.Button[3]').click()
+        time.sleep(2)
+        self.driver.find_element_by_id('com.android.permissioncontroller:id/permission_allow_button').click()
+        time.sleep(2)
+
+    def select_photo(self):
+        self.driver.tap([(113, 1751), (967, 1877)], 500)
+        self.driver.tap([(113, 1751), (967, 1877)], 500)
+        time.sleep(2)
+
+    def select_done(self):
+        self.driver.tap([(1006, 145)], 50)
+        time.sleep(3)
+
+    def change_id(self):
+        self.driver.find_element_by_class_name('android.widget.EditText').click()
+        self.driver.find_element_by_class_name('android.widget.EditText').clear()
+        self.driver.find_element_by_class_name('android.widget.EditText').send_keys(self.random_number())
+        self.driver.tap([(151, 418)], 50)
